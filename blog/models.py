fce2e4f33e9e713dd9075931662b2a4887387444
charmantemort <dawnwestmorland45@gmail.com>
2024-11-02 08:55:21 +0400

    image

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,11 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    photo = models.ImageField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -16,3 +16,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Photo(models.Model):
+        title = models.CharField(max_length=255, blank=False, null=False)
+        photo = models.ImageField(upload_to='Photos/', null=True, max_length=255)
+
+        def repr(self):
+            return 'Photo(%s, %s)' % (self.title, self.photo)
+
+        def str(self):
+            return self.title
+
+    class Item(models.Model):
+        name = models.CharField(max_length=200)
+        body = models.TextField(max_length=500)
+
+        date = models.DateTimeField(default=timezone.now)
